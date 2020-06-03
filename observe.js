565d58047b03e9766573e3c081e441e787ac8adf
uuq <42138175+CreatMyName@users.noreply.github.com>
2020-06-03 10:10:04 +0800

    修复部分功能性bug
    修复数组新增修改数据无法调用渲染方法问题，修复多对象驱动渲染方法异常问题

--- observe.js
+++ observe.js
@@ -1,23 +1,27 @@
-/*
- * @Author: hou
- * @Date: 2019-11-18 15:40:01 
- * @Last Modified by: hou
- * @Last Modified time: 2019-12-24 14:06:48
- */
-const drive = function(){
-    let page = null
+class DataDrive{
+    constructor(obj){
+        if(!obj) return false
+        'data' in obj ? obj.data = this.resolve(obj.data) : null
+        'render' in obj ? this.renderFun = obj.render : null
+        typeof this.renderFun === 'function' ? this.renderFun() : null
+    }
+    $set(data, key, targit, render){
+        targit[key] = this.resolve(data, key, targit)
+        this.renderFun = render || null
+    }
     /**
      * defin 观察发布
      * @param { String } type 支持object和array两个参数
      * @param { Object } data 拦截的对象
      * @param { String } key 拦截的字段名或方法名
      * @param { more } val 拦截参数或value方法
-     */   
-    function define(isarr,data,key,val){
+     */  
+    define(isarr,data,key,val){
         let handle = {
             enumerable:true,
             configurable:true
         }
+        let _this = this
         if(!isarr){
             handle.get = function (){
                 return val
@@ -27,89 +31,96 @@ const drive = function(){
                 let type = typeof val !== typeof newval
                 /** 判断set值 方便做二次监听 */
                 if(type && typeof newval === 'object'){
-                    Array.isArray(newval) ? observeArr(newval) : newval = proxy(newval)
+                    Array.isArray(newval) ? _this.observeArr(newval) : newval = _this.proxy(newval)
                 }
                 val = newval
                 /* 渲染 */
-                render('from defin object')
+                _this.render('from defin object')
                 return 
             }
         }else{
             handle.writable = true
             handle.value = val || function(...args){
+                if(key === 'push'){
+                    args = _this.proxy(args)
+                }
                 const res = Array.prototype[key].apply(this, args)
                 /* 渲染 */
-                render('from defin array')
+                _this.render('from defin array')
                 return res
             }
         }
         Object.defineProperty(data,key,handle)
     }
-
     /**
      * 数组拦截
      * @param { Array } target  
      */
-    function observeArr(target){
+    observeArr(target){
+        /** 继续监听子集 */
+        target.forEach((item,index) => target[index] = this.resolve(item))
         /** 拦截数组的方法 */
         const interceptMethods = ['push', 'pop', 'shift', 'unshift', 'splice', 'reverse', 'sort']
         /** 创建Array对象，由Array的原型创建 */
         const arrayMethods = Object.create(Array.prototype)
         /** 逐个设置拦截 */
-        interceptMethods.forEach(item => define(true,  arrayMethods, item))
+        interceptMethods.map(item => this.define(true,  arrayMethods, item))
         /** __proto__指向被拦截的数组原型 */
-        '__proto__' in {} ? target.__proto__ = arrayMethods : interceptMethods.forEach(item => define(true, target, interceptMethods[item], arrayMethods[interceptMethods[item]]))
+        '__proto__' in {} ? target.__proto__ = arrayMethods : interceptMethods.map(item => this.define(true, target, interceptMethods[item], arrayMethods[interceptMethods[item]]))
+        return target
     }
-
-    /** 代理拦截 */
-    function proxy(value){
-        initDrive(value)
+    /**
+     * 数组拦截
+     * @param { Array } target  
+     */
+    proxy(value){
+        this.initDrive(value)
+        let _that = this
         return new Proxy(value,{
             set(targit,prot,val,receiver){
                 let isset = Reflect.set(targit,prot,val,receiver)
                 /**新值进行拦截 */
                 if(typeof targit[prot] === 'object'){
-                    Array.isArray(targit[prot]) ? observeArr(targit[prot]) : val = proxy(targit[prot])
+                    Array.isArray(targit[prot]) ? _that.observeArr(targit[prot]) : val = _that.proxy(targit[prot])
                     isset = Reflect.set(targit,prot,val,receiver)
                 }else{
-                    define(false, targit, prot, val)
+                    _that.define(false, targit, prot, val)
                 }
                 /**自定义操作 */
-                render('from proxy')
+                _that.render('from proxy')
                 return isset
             }
         })
     }
-    
     /** 初始化数据拦截 */
-    function initDrive(data){
+    initDrive(data){
         for(var key in data){
             data[key] === null || data[key] === undefined ? data[key] = '' : null
             if(typeof data[key] === 'object'){
-                Array.isArray(data[key]) ? observeArr(data[key]) : data[key] = proxy(data[key])
+                Array.isArray(data[key]) ? this.observeArr(data[key]) : data[key] = this.proxy(data[key])
             }else{
-                define(false, data, key, data[key])
+                this.define(false, data, key, data[key])
             }
         }
     }
-
+    /** 类型 */
+    resolve(data, key, targit) {
+        if(typeof data !== 'object'){
+            key && targit ? this.define(false, targit, key, data[key]) : null
+            return data
+        }
+        if(Array.isArray(data)) 
+            return this.observeArr(data)
+        return this.proxy(data)
+    }
     /** 订阅者 */
-    function render(from){
-        // console.log(page.data)
+    render(from){
         console.log('渲染一下！' + from)
+        typeof this.renderFun === 'function' ? this.renderFun() : null;
     }
-
-    /** 驱动器对外接口 */
-    class DataDrive{
-        constructor(page){
-            page = page
-            page.data = proxy(page.data)
-        }
-        $set(data,key,value){
-            console.warn(`此功能接口暂未开放，敬请期待~`)
-        }
-    }
-
-    return DataDrive
 }
-// export default drive()
+
+export default function(obj){
+    let drive = drive || new DataDrive(obj)
+    return drive
+}
